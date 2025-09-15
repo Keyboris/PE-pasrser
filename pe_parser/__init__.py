77da981f8b04ecac3645a0ab98e72f29395a9c68
Keyboris <iluyaretskyi@gmai.com>
2025-09-15 23:38:58 +0100

    Bug fix

--- pe_parser/__init__.py
+++ pe_parser/__init__.py
@@ -4,15 +4,15 @@ PE Parser - A Python library for parsing Portable Executable (PE) files.
 
 __version__ = "0.1.0"
 
-from .models import SectionHeader, ImageImportDescriptor
-from .unpackers import (
-    dos_header_unpack,
+from pe_parser.models import section_header, image_import_descriptor
+from pe_parser.unpackers import (
+    DOS_header_unpack,
     image_file_header_unpack,
     optional_header_unpack,
     section_headers_unpack,
     import_directory_table_unpack
 )
-from .utils import rva_to_file_offset
+from pe_parser.utils import rva_to_file_offset
 
 __all__ = [
     # Models
