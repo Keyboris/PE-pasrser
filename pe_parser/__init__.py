d238bd9b1b4d7ad295521ba74413d31cf9bca176
Keyboris <iluyaretskyi@gmai.com>
2026-04-18 18:57:54 +0100

    updated to match a new unpacker structure, include feature extraction

--- pe_parser/__init__.py
+++ pe_parser/__init__.py
@@ -2,30 +2,23 @@
 PE Parser - A Python library for parsing Portable Executable (PE) files.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from pe_parser.models import SectionHeader, ImageImportDescriptor
-from pe_parser.unpacker import (
-    DOS_header_unpack,
-    COFF_header_unpack,
-    optional_header_unpack,
-    section_headers_unpack,
-    import_directory_table_unpack
-)
-from pe_parser.utils import rva_to_file_offset
+from pe_parser.unpacker import Unpacker
+from pe_parser.feature_extractor import FeatureExtractor
+from pe_parser.utils import rva_to_file_offset, section_name_by_rva
 
 __all__ = [
     # Models
     'SectionHeader',
     'ImageImportDescriptor',
     
-    # Unpacker functions
-    'DOS_header_unpack',
-    'COFF_header_unpack',
-    'optional_header_unpack',
-    'section_headers_unpack',
-    'import_directory_table_unpack',
+    # Core Classes
+    'Unpacker',
+    'FeatureExtractor',
     
     # Utility functions
     'rva_to_file_offset',
+    'section_name_by_rva'
 ]
