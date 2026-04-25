6fdf5b15281700739686c28ca252f1fc944ab5a4
Keyboris <iluyaretskyi@gmail.com>
2026-04-25 21:11:44 +0100

    update init.py

--- pe_parser/__init__.py
+++ pe_parser/__init__.py
@@ -6,7 +6,12 @@ __version__ = "0.2.0"
 
 from pe_parser.models import SectionHeader, ImageImportDescriptor
 from pe_parser.unpacker import Unpacker
-from pe_parser.feature_extractor import FeatureExtractor
+from pe_parser.headers_feature_extractor import HeadersFeatureExtractor
+from pe_parser.section_feature_extractor import SectionFeatureExtractor
+from pe_parser.import_feature_extractor import ImportFeatureExtractor
+from pe_parser.constants.header_constants import DOS_HEADER_FORMAT, DOS_HEADER_KEYS, COFF_HEADER_FORMAT, COFF_HEADER_KEYS, OPTIONAL_HEADER_64_KEYS, OPTIONAL_HEADER_64_FORMAT, OPTIONAL_HEADER_32_FORMAT, OPTIONAL_HEADER_32_KEYS
+from pe_parser.constants.suspicious_apis_constants import SUSPICIOUS_APIS
+from pe_parser.entropy_calculator import section_entropy
 from pe_parser.utils import rva_to_file_offset, section_name_by_rva
 
 __all__ = [
@@ -16,9 +21,24 @@ __all__ = [
     
     # Core Classes
     'Unpacker',
-    'FeatureExtractor',
+    'HeadersFeatureExtractor',
+    'SectionFeatureExtractor',
+    'ImportFeatureExtractor',
+
     
     # Utility functions
     'rva_to_file_offset',
-    'section_name_by_rva'
+    'section_name_by_rva',
+    'section_entropy',
+
+    # Constants
+    'DOS_HEADER_FORMAT',
+    'DOS_HEADER_KEYS',
+    'COFF_HEADER_FORMAT',
+    'COFF_HEADER_KEYS',
+    'OPTIONAL_HEADER_64_KEYS',
+    'OPTIONAL_HEADER_64_FORMAT',
+    'OPTIONAL_HEADER_32_FORMAT',
+    'OPTIONAL_HEADER_32_KEYS',
+    'SUSPICIOUS_APIS'
 ]
