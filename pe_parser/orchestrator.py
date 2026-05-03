074eb5613c055e07487de4eb1980a1120388e4c4
Keyboris <iluyaretskyi@gmail.com>
2026-05-03 11:15:40 +0100

    add is_dll

--- pe_parser/orchestrator.py
+++ pe_parser/orchestrator.py
@@ -1,6 +1,7 @@
 from os.path import join
 from os import walk
 from pe_parser.analyzer import process_file
+import os
 
 def process_destination(dirpath: str, recursive: bool) -> list[dict]:
     results = []
@@ -10,6 +11,7 @@ def process_destination(dirpath: str, recursive: bool) -> list[dict]:
             result = process_file(full_path)
             if result is not None:
                 result["filepath"] = full_path
+                result["is_dll"] = True if os.path.splitext(full_path) == ".dll" else False
                 print(f"processed: {full_path}")
                 results.append(result)
         if not recursive:
